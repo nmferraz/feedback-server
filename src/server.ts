56eeb595fd9b4e0b682c4e4f6f93fd266de4ba57
Nuno Miguel Ferraz <108nunomiguelferraz@gmail.com>
2022-05-04 15:17:38 +0100

    feedback route

--- src/server.ts
+++ src/server.ts
@@ -1,12 +1,21 @@
+import { prisma } from "./prisma";
 import express from "express";
 
 const app = express();
 
 app.use(express.json());
 
-app.post("/feedbacks", (req, res) => {
-  console.log(req.body);
-  return res.send("Feedback received");
+app.post("/feedbacks", async (req, res) => {
+  const { type, comment, screenshot } = req.body;
+
+  const feedback = await prisma.feedback.create({
+    data: {
+      type,
+      comment,
+      screenshot,
+    },
+  });
+  return res.status(201).json({ data: feedback });
 });
 
 app.listen(3333, () => {
