6c84c658160dfe3f170595daa3c541a1583b9e35
Nuno Miguel Ferraz <108nunomiguelferraz@gmail.com>
2022-05-04 15:08:48 +0100

    create basic route

--- src/server.ts
+++ src/server.ts
@@ -1,7 +1,14 @@
-import express from 'express';
+import express from "express";
 
 const app = express();
 
-app.listen(3333, () =>{
-    console.log(' HTTP server running on port 3333!');
-})
+app.use(express.json());
+
+app.post("/feedbacks", (req, res) => {
+  console.log(req.body);
+  return res.send("Feedback received");
+});
+
+app.listen(3333, () => {
+  console.log(" HTTP server running on port 3333!");
+});
