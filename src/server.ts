69f3a0bc942b20716d8d2c76984459efe2167298
Nuno Miguel Ferraz <108nunomiguelferraz@gmail.com>
2022-05-11 14:28:20 +0100

    parameterLimit again

--- src/server.ts
+++ src/server.ts
@@ -6,7 +6,13 @@ import bodyParser from "body-parser";
 const app = express();
 
 app.use(bodyParser.json({ limit: "50mb" }));
-app.use(bodyParser.urlencoded({ limit: "50mb", extended: true }));
+app.use(
+  bodyParser.urlencoded({
+    limit: "50mb",
+    extended: true,
+    parameterLimit: 50000,
+  })
+);
 app.use(cors());
 app.use(express.json());
 app.use(routes);
