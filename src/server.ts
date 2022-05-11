8f8b3fea9822e6d7dbf2533c3d5f5d8e0dd71148
Nuno Miguel Ferraz <108nunomiguelferraz@gmail.com>
2022-05-11 11:38:41 +0100

    parameterLimit

--- src/server.ts
+++ src/server.ts
@@ -5,11 +5,11 @@ import bodyParser from "body-parser";
 
 const app = express();
 
-app.use(bodyParser.json({limit: '50mb'}));
-app.use(bodyParser.urlencoded({limit: '50mb', extended: true}));
-app.use(cors());
+app.use(bodyParser.json({ limit: "50mb" }));
+app.use(bodyParser.urlencoded({ limit: "50mb", extended: true, parameterLimit: 50000 }));
 app.use(express.json());
-app.use(routes); 
+app.use(cors());
+app.use(routes);
 
 app.listen(process.env.PORT || 3333, () => {
   console.log(" HTTP server running on port 3333!");
