f8d89a5b64e81a9f0504098fa9cad256509d1692
Nuno Miguel Ferraz <108nunomiguelferraz@gmail.com>
2022-05-11 11:35:00 +0100

    bodyParser

--- src/server.ts
+++ src/server.ts
@@ -1,9 +1,12 @@
 import express from "express";
 import cors from "cors";
 import { routes } from "./routes";
+import bodyParser from "body-parser";
 
 const app = express();
 
+app.use(bodyParser.json({limit: '50mb'}));
+app.use(bodyParser.urlencoded({limit: '50mb', extended: true}));
 app.use(cors());
 app.use(express.json());
 app.use(routes); 
