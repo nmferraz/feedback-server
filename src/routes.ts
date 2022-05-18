e7944f32758e3ab9f44fcb01ce2b1e6e25c50f2a
Nuno Miguel Ferraz <108nunomiguelferraz@gmail.com>
2022-05-18 20:59:45 +0100

    new try

--- src/routes.ts
+++ src/routes.ts
@@ -35,7 +35,7 @@ routes.post("/feedbacks", async (req, res) => {
 routes.get("/allfeedbacks", async (req, res) => {
   try {
     const feedbacks = await prisma.feedback.findMany();
-    res.json(feedbacks);
+    return res.json(feedbacks);
   } catch (error) {
     console.log(error);
   }
